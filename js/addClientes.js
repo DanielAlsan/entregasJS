2f115ed81acb2c80c47b1feae64ba562da55cb4a
Albino <danielsanchezmaya@gmail.com>
2022-08-11 13:07:04 -0500

    obtener datos en tablas por json

--- js/addClientes.js
+++ js/addClientes.js
@@ -1,18 +1,16 @@
-const URL ='/js/data/products.json'
+const URL ='/js/data/clients.json'
 
 fetch(URL)
   .then(response => response.json())
-  .then(data => mostrarData(data))
+  .then(data => mostrarCliente(data))
   .catch(error => console.log(error));
 
-const mostrarData = (data)=>{
+const mostrarCliente = (data)=>{
   console.log(data);
   let body = ''
   for(let i =0;i <data.length;i++){
-    body +=`<tr><td>${data[i].id}</td><td>${data[i].arbol}</td><td>${data[i].costo}</td></tr>`
+    body +=`<tr><td>${data[i].id}</td><td>${data[i].nombre}</td><td>${data[i].ubicacion}</td></tr>`
   }
   document.getElementById('data').innerHTML=body
   document.getElementById('datoActual').innerHTML=data.length+1
 }
-
-
