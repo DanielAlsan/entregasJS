ecbed9d76c7e40f9931666ae5fc20629fa2bd441
Albino <danielsanchezmaya@gmail.com>
2022-08-19 09:55:10 -0500

    cambios visuales

--- js/addClientes.js
+++ js/addClientes.js
@@ -9,7 +9,7 @@ const mostrarCliente = (data)=>{
   console.log(data);
   let body = ''
   for(let i =0;i <data.length;i++){
-    body +=`<tr><td>${data[i].id}</td><td>${data[i].nombre}</td><td>${data[i].ubicacion}</td></tr>`
+    body +=`<tr><td>${data[i].id}</td><td>${data[i].nombre}</td><td>${data[i].ubicacion}</td><td><button class="btn btn-light" type="submit">Editar</button><button class="btn btn-light" type="submit">Eliminar</button></td></tr>`
   }
   document.getElementById('data').innerHTML=body
   document.getElementById('datoActual').innerHTML=data.length+1
